56fce02c17a868eba1752464ffebe6dae34bd24f
XenoDEV <DragonWolfHD2015@gmail.com>
2021-09-02 22:20:07 +0200

    SrandardCMD hinzugefügt

--- src/main/java/me/xenodev/sp/utils/player/RespawnItemBuilder.java
+++ src/main/java/me/xenodev/sp/utils/player/RespawnItemBuilder.java
@@ -9,10 +9,10 @@ public class RespawnItemBuilder {
 
     public static void setStandardItems(Player p){
 
-        p.getInventory().setHelmet(new ItemBuilder(Material.LEATHER_HELMET).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
-        p.getInventory().setChestplate(new ItemBuilder(Material.LEATHER_CHESTPLATE).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
-        p.getInventory().setLeggings(new ItemBuilder(Material.LEATHER_LEGGINGS).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
-        p.getInventory().setBoots(new ItemBuilder(Material.LEATHER_BOOTS).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_HELMET).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_CHESTPLATE).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_LEGGINGS).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
+        p.getInventory().addItem(new ItemBuilder(Material.LEATHER_BOOTS).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
 
         p.getInventory().addItem(new ItemBuilder(Material.WOOD_SWORD).setEnchantment(Enchantment.DAMAGE_ALL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
         p.getInventory().addItem(new ItemBuilder(Material.COOKED_BEEF).setAmount(16).setName("§e§lSkyPvP §8× §7Starter").build());
