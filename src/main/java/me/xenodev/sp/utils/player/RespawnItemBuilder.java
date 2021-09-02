cb849324786ed12713efdef242c863f835d366b8
XenoDEV <DragonWolfHD2015@gmail.com>
2021-09-02 22:23:11 +0200

    SrandardCMD hinzugefügt

--- src/main/java/me/xenodev/sp/utils/player/RespawnItemBuilder.java
+++ src/main/java/me/xenodev/sp/utils/player/RespawnItemBuilder.java
@@ -8,15 +8,12 @@ import org.bukkit.entity.Player;
 public class RespawnItemBuilder {
 
     public static void setStandardItems(Player p){
-
+        p.getInventory().addItem(new ItemBuilder(Material.WOOD_SWORD).setEnchantment(Enchantment.DAMAGE_ALL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
+        p.getInventory().addItem(new ItemBuilder(Material.COOKED_BEEF).setAmount(16).setName("§e§lSkyPvP §8× §7Starter").build());
         p.getInventory().addItem(new ItemBuilder(Material.LEATHER_HELMET).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
         p.getInventory().addItem(new ItemBuilder(Material.LEATHER_CHESTPLATE).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
         p.getInventory().addItem(new ItemBuilder(Material.LEATHER_LEGGINGS).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
         p.getInventory().addItem(new ItemBuilder(Material.LEATHER_BOOTS).setEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
-
-        p.getInventory().addItem(new ItemBuilder(Material.WOOD_SWORD).setEnchantment(Enchantment.DAMAGE_ALL, 1).setName("§e§lSkyPvP §8× §7Starter").build());
-        p.getInventory().addItem(new ItemBuilder(Material.COOKED_BEEF).setAmount(16).setName("§e§lSkyPvP §8× §7Starter").build());
-
     }
 
 }
